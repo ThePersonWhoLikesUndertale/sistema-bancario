f1d5a918b25ed9643a72ddef110bd80f58fc711e
TPWLU <sppaulisto972@gmail.com>
2025-02-26 15:35:51 -0300

    Fix: parse opcao, deposito and saque to float

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -15,43 +15,39 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-    opcao = input(menu)
+    opcao = float(input(menu))
     
-    if opcao == "1":
-        deposito = input("""Digite o valor que deseja depositar.
-                         => """)
+    if opcao == 1:
+        deposito = float(input("""Digite o valor que deseja depositar.
+=> """))
         if deposito > 0:
             saldo += deposito
-            extrato += f"""Depósito
-            Valor: R$ {deposito:.2f}
-            """
+            extrato += f"Depósito\nValor: R$ {deposito:.2f}\n\n"
             print("Depósito bem sucedido!")
         else:
             print("Valor inválido, por favor tente novamente.")
         
-    elif opcao == "2":
-        saque = input("""Digite o valor que deseja sacar.
-                      => """)
+    elif opcao == 2:
+        saque = float(input("""Digite o valor que deseja sacar.
+=> """))
         if saque <= saldo and saque <= limite and saque > 0 and numero_saques < LIMITE_SAQUES:
             saldo -= saque
-            extrato += f"""Saque
-            Valor: R$ {saque:.2f}
-            """
+            extrato += f"Saque\nValor: R$ {saque:.2f}\n\n"
             numero_saques += 1
             print("Saque bem sucedido!")
-        elif numero_saques > LIMITE_SAQUES:
+        elif numero_saques >= LIMITE_SAQUES:
             print("Você esgotou seus saques diários! Tente novamente outro dia.")
-        elif saque > saldo:
-            print("Seu saldo atual é insuficiente.")
         elif saque > limite:
             print("Seu saque não pode ultrapassar R$ 500.00. Por favor, insira outro valor.")
+        elif saque > saldo:
+            print("Seu saldo atual é insuficiente.")
         else:
             print("Valor inválido. Por favor, tente novamente.")
             
-    elif opcao == "3":
+    elif opcao == 3:
         print(extrato+f"Saldo atual: R$ {saldo:.2f}")
     
-    elif opcao == "0":
+    elif opcao == 0:
         break
     
     else:

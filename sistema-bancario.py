120fc4afc618d5df7385f03adf2efc1548378c45
TPWLU <sppaulisto972@gmail.com>
2025-02-26 14:25:46 -0300

    Update: Finish system loop template

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -16,5 +16,18 @@ LIMITE_SAQUES = 3
 
 while True:
     opcao = input(menu)
-    if opcao == 0:
+    
+    if opcao == 1:
+        print("Depósito")
+        
+    elif opcao == 2:
+        print("Saque")
+    
+    elif opcao == 3:
+        print("Extrato")
+    
+    elif opcao == 0:
         break
+    
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")

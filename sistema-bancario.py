09b8aeefcb621f5556f62d80af8269eeae7d5b55
TPWLU <sppaulisto972@gmail.com>
2025-02-26 14:21:46 -0300

    Fix: Remove redundant variable and optimize system loop

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -6,15 +6,15 @@ menu = """
     [2] Sacar
     [3] Extrato
     [0] Sair
-"""
+    
+    => """
 saldo = 0
-sistema = True
 limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-while(sistema == True):
+while True:
     opcao = input(menu)
-    if (opcao == 0):
-        sistema = False
+    if opcao == 0:
+        break
